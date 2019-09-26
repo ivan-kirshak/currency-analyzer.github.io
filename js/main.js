4a7119b31e5704f9c8004237173fd4a5c1acd067
Ivan Kirshak <i.kirshak@gmail.com>
2019-09-26 21:18:53 +0300

    added first part of functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // currency identificators
-
 let firstCurrency = document.getElementById("currencyOne");
 let secondCurrency = document.getElementById("currencyTwo");
 
@@ -9,6 +8,42 @@ let secondDate = document.getElementById("dateTwo");
 
 // search button
 let searchBtn = document.getElementById("search");
+let resultOne = document.getElementById("resultOne");
+let resultTwo = document.getElementById("resultTwo");
+searchBtn.addEventListener("click", function (e) {
+    if (e.target.value == "") {
+        return;
+    }
+    let firstCorrect = firstDate.value.split("-").join("");
+    let secondCorrect = secondDate.value.split("-").join("");
+
+    // Request for first currency
+    const XHR_One = new XMLHttpRequest();
+    let URI_One = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${firstCurrency.value}&date=${firstCorrect}&json`;
+    
+    XHR_One.addEventListener("readystatechange", function () {
+        if ((XHR_One.readyState === 4) && (XHR_One.status === 200)) {
+            let res = JSON.parse(XHR_One.responseText);
+            resultOne.innerHTML = `1 ${firstCurrency.value} = ${res[0].rate.toFixed(2)} hryvnias`;
+        }
+    }, false);
+    XHR_One.open("GET", URI_One);
+    XHR_One.send()
+
+    // Request for second currency 
+    const XHR_Two = new XMLHttpRequest();
+    let URI_Two = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${secondCurrency.value}&date=${secondCorrect}&json`;
+    
+    XHR_Two.addEventListener("readystatechange", function () {
+        if ((XHR_Two.readyState === 4) && (XHR_Two.status === 200)) {
+            let res = JSON.parse(XHR_Two.responseText);
+            resultTwo.innerHTML = `1 ${secondCurrency.value} = ${res[0].rate.toFixed(2)} hryvnias`;
+        }
+    }, false);
+    XHR_Two.open("GET", URI_Two);
+    XHR_Two.send()
+
+}, false)
 
 // chart
 let chart = document.getElementById("chart");
