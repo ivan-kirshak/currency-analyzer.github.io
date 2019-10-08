571bf2c40cfd778639280ace32090f50741102ab
Ivan Kirshak <i.kirshak@gmail.com>
2019-10-08 15:43:39 +0300

    updated structure of page

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-// quantity identificators
-let firstQuantity = document.getElementById("quantityOne");
-let secondQuantity = document.getElementById("quantityTwo");
+// UPPER PART OF PAGE (RATE GRAPH)
 
-// currency identificators
-let firstCurrency = document.getElementById("currencyOne");
-let secondCurrency = document.getElementById("currencyTwo");
+// currency identificator
+let currency = document.getElementById("currency");
 
 // date identificators
 let firstDate = document.getElementById("dateOne");
 let secondDate = document.getElementById("dateTwo");
 
 // search button
-let searchBtn = document.getElementById("search");
-let resultOne = document.getElementById("resultOne");
-let resultTwo = document.getElementById("resultTwo");
+let searchBtn = document.getElementById("analyze");
 
 let dataArray = [];
 
@@ -26,17 +21,13 @@ function displayRate (e) {
     let firstCorrect = firstDate.value.split("-").join("");
     let secondCorrect = secondDate.value.split("-").join("");
 
-    let valueOne = firstQuantity.value;
-    let valueTwo = secondQuantity.value;
-
-    /*
     for (let i = Number(firstCorrect); i <= Number(secondCorrect); i++) {
         let XHR = new XMLHttpRequest();
-        let URI = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${firstCurrency.value}&date=${i}&json`;
+        let URI = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${currency.value}&date=${i}&json`;
         XHR.addEventListener("readystatechange", function() {
             if ((XHR.readyState === 4) && (XHR.status === 200)) {
-                let result = JSON.parse(XHR.responseText);
-                let resObj = {rate: result[0].rate, date: result[0].exchangedate};
+                let res = JSON.parse(XHR.responseText);
+                let resObj = {rate: res[0].rate, date: res[0].exchangedate};
                 dataArray.push(resObj);
                 console.log(dataArray);
             }
@@ -44,96 +35,88 @@ function displayRate (e) {
         XHR.open("GET", URI);
         XHR.send(); 
     }
-    */
-
-    
-    // Request for first currency
-    const XHR_One = new XMLHttpRequest();
-    let URI_One = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${firstCurrency.value}&date=${firstCorrect}&json`;
-    
-    XHR_One.addEventListener("readystatechange", function () {
-        if ((XHR_One.readyState === 4) && (XHR_One.status === 200)) {
-            let res = JSON.parse(XHR_One.responseText);
-            resultOne.innerHTML = `${valueOne} ${firstCurrency.value} = ${valueOne * res[0].rate.toFixed(2)} hryvnias (${firstDate.value})`;
-        }
-    }, false);
-    XHR_One.open("GET", URI_One);
-    XHR_One.send();
-
-    
-    // Request for second currency 
-    const XHR_Two = new XMLHttpRequest();
-    let URI_Two = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${secondCurrency.value}&date=${secondCorrect}&json`;
-    
-    XHR_Two.addEventListener("readystatechange", function () {
-        if ((XHR_Two.readyState === 4) && (XHR_Two.status === 200)) {
-            let res = JSON.parse(XHR_Two.responseText);
-            resultTwo.innerHTML = `${valueTwo} ${secondCurrency.value} = ${valueTwo * res[0].rate.toFixed(2)} hryvnias (${secondDate.value})`;
-        }
-    }, false);
-    XHR_Two.open("GET", URI_Two);
-    XHR_Two.send(); 
-    
 }
 
-// CHART
-function displayChart() {
-    Highcharts.chart('chart', {
+let chartOptions = {     
+    title: {
+        text: `Currency rate of ${currency.value} from ${firstDate.value} to ${secondDate.value}`
+    },
 
+    yAxis: {
         title: {
-            text: `Comparison of ${firstCurrency.value} to ${secondCurrency.value} from ${firstDate.value} to ${secondDate.value}`
-        },
-    
-        yAxis: {
-            title: {
-                text: 'Currency rate'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-    
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: `${firstDate.value}`
-            }
-        },
-
-        series: [{
-            name: `${firstCurrency.value}`,
-            data: `${dataArray}`
-        }, {
-            name: `${secondCurrency.value}`,
-            data: `${secondDate}`
-        }],
-    
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
+            text: 'Currency rate'
         }
-    });
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: `${firstDate.value}`
+        }
+    },
+
+    series: [{
+        name: `${currency.value}`,
+        data: `${dataArray}`
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
 }
 
 searchBtn.addEventListener("click", displayRate, false);
-searchBtn.addEventListener("click", displayChart, false);
+Highcharts.chart("chart", chartOptions)
 
 // chart
 let chart = document.getElementById("chart");
+
+// END OF UPPER PART (CURRENCY GRAPH)
+
+//  LOWER PART (CURRENCY CONVERTER)
+let currencyConv = document.getElementById("currencyConv");
+let dateConv = document.getElementById("dateConv");
+let converter = document.getElementById("convert");
+let quantity = document.getElementById("quantity");
+let correctQuantity = Number(quantity.value);
+let resultOne = document.getElementById("resultConv")
+
+function convertMoney() {
+    let correctDate = dateConv.value.split("-").join("");
+
+    const XHR_Conv = new XMLHttpRequest();
+    let URI_Conv = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${currencyConv.value}&date=${correctDate}&json`;
+    
+    XHR_Conv.addEventListener("readystatechange", function () {
+        if ((XHR_Conv.readyState === 4) && (XHR_Conv.status === 200)) {
+            let res = JSON.parse(XHR_Conv.responseText);
+            resultConv.innerHTML = `${correctQuantity} ${currencyConv.value} = ${correctQuantity * res[0].rate.toFixed(2)} hryvnias (${dateConv.value})`;
+        }
+    }, false);
+    XHR_Conv.open("GET", URI_Conv);
+    XHR_Conv.send();
+}
+
+converter.addEventListener("click", convertMoney, false)
+//  END OF LOWER PART (CURRENCY CONVERTER)
 
 // to Top button in footer
 const toTopBtn = document.getElementById("toTopBtn");
